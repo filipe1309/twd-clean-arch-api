57cc25dad3f0348326ad30b04ff97fb7e20a87d9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-12-19 15:27:28 -0300

    feat: init first test

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usercase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = 'John Doe'
+    // const email = 'john.doe@test.com'
+    // const response = await usercase.RegisterUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail(email)
+    // expect((await user).name).toBe(name)
+  })
+})
