ab60cac363fd77141d7b95855351ff7e6bc54bc8
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-12-22 23:24:05 -0300

    test: validate email with domain part larger than 63 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -40,4 +40,9 @@ describe('Email validation', () => {
     const email = 'local@'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept email domain with each part larger than 63 chars', () => {
+    const email = 'local@' + 'd'.repeat(64) + '.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
