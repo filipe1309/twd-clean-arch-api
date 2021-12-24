c8e5efe699d0edbc847dd40757db65433390d066
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-12-24 14:20:06 -0300

    feat: either type and right and left classes

---
+++ src/shared/either.ts
@@ -0,0 +1,39 @@
+export class Left<L, A> {
+  readonly _tag: 'Left' = 'Left'
+  readonly value: L
+
+  constructor (value: L) {
+    this.value = value
+  }
+
+  isLeft (): this is Left<L, A> {
+    return true
+  }
+
+  isRight (): this is Right<L, A> {
+    return false
+  }
+}
+
+export class Right<L, A> {
+  readonly _tag: 'Right' = 'Right'
+  readonly value: A
+
+  constructor (value: A) {
+    this.value = value
+  }
+
+  isLeft (): this is Left<L, A> {
+    return false
+  }
+
+  isRight (): this is Right<L, A> {
+    return true
+  }
+}
+
+export type Either<L, A> = Left<L, A> | Right<L, A>
+
+export const left = <L, A>(l: L): Either<L, A> => new Left<L, A>(l)
+
+export const right = <L, A>(a: A): Either<L, A> => new Right<L, A>(a)
