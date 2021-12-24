b653a6fc16b37a4f08eddfa0f99fe0e81606ba03
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-12-24 14:44:48 -0300

    test: verify name while creating user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,11 +1,24 @@
 import { User } from './user'
 import { left } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
 
 describe('User domain entity', () => {
   test('should not create user with invalid e-mail address', () => {
     const invalidEmail = 'invalid-email'
     const error = User.create({ name: 'John Doe', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('should not create user with invalid name', () => {
+    const invalidName = '0      '
+    const error = User.create({ name: invalidName, email: 'john.doe@test.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
+  })
+
+  test('should not create user with a large name', () => {
+    const invalidName = '0'.repeat(257)
+    const error = User.create({ name: invalidName, email: 'john.doe@test.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })
