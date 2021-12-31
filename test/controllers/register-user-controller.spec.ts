960a466d96584ee22b813a8f96b4a90aeb7e8acb
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-12-31 18:50:10 -0300

    test: controller should return 400 when user has invalid name or email

--- test/controllers/register-user-controller.spec.ts
+++ test/controllers/register-user-controller.spec.ts
@@ -1,6 +1,7 @@
 import { HttpRequest, HttpResponse } from '@/controllers/ports'
 import { RegisterUserController } from '@/controllers/register-user-controller'
 import { UserData } from '@/entities'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
 import { InMemoryUserRepository } from '@test/usercases/register-user-on-mailing-list/repository'
@@ -15,15 +16,45 @@ describe('Register user with web controller', () => {
     }
     const users: UserData[] = []
     const repo: UserRepository = new InMemoryUserRepository(users)
-    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
-      repo
-    )
-    const controller: RegisterUserController = new RegisterUserController(
-      usecase
-    )
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const controller: RegisterUserController = new RegisterUserController(usecase)
     const response: HttpResponse = await controller.handle(request)
 
     expect(response.statusCode).toBe(201)
     expect(response.body).toEqual(request.body)
+  })
+
+  test('should return status code 400 when request contains a invalid name', async () => {
+    const request: HttpRequest = {
+      body: {
+        name: 'J',
+        email: 'john.doe@test.com'
+      }
+    }
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const controller: RegisterUserController = new RegisterUserController(usecase)
+    const response: HttpResponse = await controller.handle(request)
+
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toBeInstanceOf(InvalidNameError)
+  })
+
+  test('should return status code 400 when request contains a invalid email', async () => {
+    const request: HttpRequest = {
+      body: {
+        name: 'John Doe',
+        email: '@test.com'
+      }
+    }
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const controller: RegisterUserController = new RegisterUserController(usecase)
+    const response: HttpResponse = await controller.handle(request)
+
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toBeInstanceOf(InvalidEmailError)
   })
 })
