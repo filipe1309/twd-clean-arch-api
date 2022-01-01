8aa760b595ed1f296fd34d89414de5ae6bcb45b9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-01-01 14:08:05 -0300

    feat: apply cors enabling middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '@/main/config/middleware/body-parser'
+import { cors } from '@/main/config/middleware/cors'
 
 export default (app: Express) => {
   app.use(bodyParser)
+  app.use(cors)
 }
