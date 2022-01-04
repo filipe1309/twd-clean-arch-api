8cd1662872ab9d05d5572462a72d735bf59b031c
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-01-04 00:55:57 -0300

    chore: add start script

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,7 @@
 import 'module-alias/register'
 import app from '@/main/config/app'
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`)
