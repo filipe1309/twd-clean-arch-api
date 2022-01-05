7a61fccc7a6aa7bdd6d37bd6f6c70a6d21b07b14
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-01-04 22:59:22 -0300

    feat: connecto to mongodb before listening

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,13 @@
 import 'module-alias/register'
-import app from '@/main/config/app'
+import { MongoHelper } from '@/external/repositories/mongodb/helper'
 
 const PORT = process.env.PORT || 5001
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`)
-})
+MongoHelper.connect(process.env.MONGO_URL!)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}/`)
+    })
+  })
+  .catch(console.error)
