8bc0611d7691a642ecfb5ddbd54d8fd95ed94fa5
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-01-04 22:04:29 -0300

    chore: add mongodb helper module

---
+++ src/external/repositories/mongodb/helper/mongodb-helper.ts
@@ -0,0 +1,21 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri)
+  },
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  },
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(process.env.MONGO_URL!)
+    }
+
+    return this.client.db().collection(name)
+  },
+  clearCollection (name: string): Promise<void> {
+    return this.getCollection(name).then(collection => collection.deleteMany({}))
+  }
+}
