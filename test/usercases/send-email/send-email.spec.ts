e46984ee7c2fa3a90a9b182287df48f1aff00c1b
Filipe Leuch Bonfim <filipe1309@gmail.com>
2022-01-12 21:48:22 -0300

    test:  make sendEmail usecase work with users

--- test/usercases/send-email/send-email.spec.ts
+++ test/usercases/send-email/send-email.spec.ts
@@ -1,7 +1,8 @@
-import { Either, left, Left, Right, right } from '@/shared'
+import { Either, left, right } from '@/shared'
 import { EmailServiceError } from '@/usecases/errors'
 import { EmailOptions, EmailService } from '@/usecases/send-email/ports'
 import { SendEmail } from '@/usecases/send-email'
+import { User } from '@/entities'
 
 const attachmentFilePath = '../resources/attachment_test.txt'
 const fromName = 'John From'
@@ -44,21 +45,16 @@ describe('Send email to user', () => {
   test('should email user with valid name and email address', async () => {
     const emailServiceStub = new EmailServiceStub()
     const usercase: SendEmail = new SendEmail(emailOptions, emailServiceStub)
-    const response = await usercase.perform({ name: toName, email: toEmail })
-    expect(response).toBeInstanceOf(Right)
-  })
-
-  test('should email user with invalid email address', async () => {
-    const emailServiceStub = new EmailServiceStub()
-    const usercase: SendEmail = new SendEmail(emailOptions, emailServiceStub)
-    const response = await usercase.perform({ name: toName, email: 'invalid' })
-    expect(response).toBeInstanceOf(Left)
+    const user = User.create({ name: toName, email: toEmail }).value as User
+    const response = (await usercase.perform(user)).value as EmailOptions
+    expect(response.to).toBe(emailOptions.to)
   })
 
   test('should return error when email service fails', async () => {
     const emailServiceErrorStub = new EmailServiceErrorStub()
     const usercase: SendEmail = new SendEmail(emailOptions, emailServiceErrorStub)
-    const response = await usercase.perform({ name: toName, email: toEmail })
+    const user = User.create({ name: toName, email: toEmail }).value as User
+    const response = await usercase.perform(user)
     expect(response.value).toBeInstanceOf(EmailServiceError)
   })
 })
